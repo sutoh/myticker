c3419be8dffef951afbc24f5a3e012444e19127a
須藤昌平 <ss.aquari@gmail.com>
2017-12-17 22:55:40 +0900

    first

---
+++ todo.js
@@ -0,0 +1,37 @@
+
+riot.tag2('todo', '<h3>{opts.title}</h3> <ul> <li each="{items.filter(whatShow)}"> <label class="{completed: done}"> <input type="checkbox" checked="{done}" onclick="{parent.toggle}"> {title} </label> </li> </ul> <form onsubmit="{add}"> <input ref="input" onkeyup="{edit}"> <button disabled="{!text}">Add #{items.filter(whatShow).length + 1}</button> <button type="button" disabled="{items.filter(onlyDone).length == 0}" onclick="{removeAllDone}"> X{items.filter(onlyDone).length} </button> </form>', '', '', function(opts) {
+    this.items = opts.items
+
+    this.edit = function(e) {
+      this.text = e.target.value
+    }.bind(this)
+
+    this.add = function(e) {
+      if (this.text) {
+        this.items.push({ title: this.text })
+        localStorage.setItem(key, value)
+        this.text = this.refs.input.value = ''
+      }
+      e.preventDefault()
+    }.bind(this)
+
+    this.removeAllDone = function(e) {
+      this.items = this.items.filter(function(item) {
+        return !item.done
+      })
+    }.bind(this)
+
+    this.whatShow = function(item) {
+      return !item.hidden
+    }.bind(this)
+
+    this.onlyDone = function(item) {
+      return item.done
+    }.bind(this)
+
+    this.toggle = function(e) {
+      var item = e.item
+      item.done = !item.done
+      return true
+    }.bind(this)
+});
